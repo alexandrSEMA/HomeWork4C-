22ce07c8ac59d76c16b0cec41dffebc54ad31b84
Alexandr <a.semashkev14@gmail.com>
2022-09-13 19:05:32 +0300

    I win this task

---
+++ Task25/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Введите число");
+int numberA =int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень возведения");
+int numberB =int.Parse(Console.ReadLine());
+int sum = numberA;
+for (int i = 1; i < numberB; i++)
+{
+    sum = sum * numberA;
+}
+System.Console.Write("Ответ = " );
+System.Console.Write(sum);
